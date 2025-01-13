0de0f9bc7f175d53d2d440177d349fcee2ea9b6f
DanielSaizVazquez <danis_998@hotmail.com>
2025-01-13 19:47:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios</title>
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+    <header>
+        <h1>Video Game Tournament</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="./paginas/inicio/inicio.php">Inicio</a></li>
+            <li><a href="./paginas/inscribirse/inscribirse.php">Inscribirse</a></li>
+            <li><a href="./paginas/creartorneo/creartorneo.php">Crear torneo</a></li>
+            <li><a href="./paginas/crearpartida/crearpartida.php">Crear partida</a></li>
+            <li><a href="./paginas/editarpartida/editarpartida.php">Editar partida</a></li>
+            <li><a href="./paginas/borrarpartida/borrarpartida.php">Borrar partida</a></li>
+            <li><a href="./paginas/crearjugador/crearjugador.php">Crear jugador</a></li>
+        </ul>
+    </nav>
+    <footer>
+    <p>Proyecto 2 - Gestión de torneos de videojuegos</p>
+    <p>Daniel Sáiz</p>
+    </footer>
+    <img src="./imagenes/ejercicio4/imagen5.png" alt="Descripción de la imagen" class="centered-image">
+</body>
+</html>
